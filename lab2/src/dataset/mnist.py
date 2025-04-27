dd96eaa77475b2e4b37158db75759f79cdb0b830
hacken <hackenzheng@126.com>
2025-04-27 11:51:15 +0800

    commit dataset script

---
+++ lab2/src/dataset/mnist.py
@@ -0,0 +1,130 @@
+# coding: utf-8
+try:
+    import urllib.request
+except ImportError:
+    raise ImportError('You should use Python 3.x')
+import os.path
+import gzip
+import pickle
+import os
+import numpy as np
+from download import download
+
+url_base = 'https://data.deepai.org/mnist.zip'
+key_file = {
+    'train_img':'MNIST_Data/train/train-images-idx3-ubyte',
+    'train_label':'MNIST_Data/train/train-labels-idx1-ubyte',
+    'test_img':'MNIST_Data/test/t10k-images-idx3-ubyte',
+    'test_label':'MNIST_Data/test/t10k-labels-idx1-ubyte'
+}
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__))
+save_file = dataset_dir + "/mnist.pkl"
+
+train_num = 60000
+test_num = 10000
+img_dim = (1, 28, 28)
+img_size = 784
+
+
+    
+def download_mnist():
+
+    if os.path.exists(dataset_dir + '/MNIST_Data/train') == False:
+        url = "https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/" \
+        "notebook/datasets/MNIST_Data.zip"
+        res = download(url, dataset_dir, kind="zip", replace=True)
+        
+def _load_label(file_name):
+    file_path = dataset_dir + "/" + file_name
+    
+    print("Converting " + file_name + " to NumPy Array ...")
+    if file_path.endswith('.gz'):
+        with gzip.open(file_path, 'rb') as f:
+                labels = np.frombuffer(f.read(), np.uint8, offset=8)
+    else:
+        with open(file_path, 'rb') as f:
+                labels = np.frombuffer(f.read(), np.uint8, offset=8)
+    print("Done")
+    
+    return labels
+
+def _load_img(file_name):
+    file_path = dataset_dir + "/" + file_name
+    
+    print("Converting " + file_name + " to NumPy Array ...")    
+    if file_path.endswith('.gz'):
+        with gzip.open(file_path, 'rb') as f:
+                data = np.frombuffer(f.read(), np.uint8, offset=16)
+    else:
+        with open(file_path, 'rb') as f:
+                data = np.frombuffer(f.read(), np.uint8, offset=16)
+    data = data.reshape(-1, img_size)
+    print("Done")
+    
+    return data
+    
+def _convert_numpy():
+    dataset = {}
+    dataset['train_img'] =  _load_img(key_file['train_img'])
+    dataset['train_label'] = _load_label(key_file['train_label'])    
+    dataset['test_img'] = _load_img(key_file['test_img'])
+    dataset['test_label'] = _load_label(key_file['test_label'])
+    
+    return dataset
+
+def init_mnist():
+    download_mnist()
+    dataset = _convert_numpy()
+    print("Creating pickle file ...")
+    with open(save_file, 'wb') as f:
+        pickle.dump(dataset, f, -1)
+    print("Done!")
+
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 10))
+    for idx, row in enumerate(T):
+        row[X[idx]] = 1
+        
+    return T
+    
+
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    """读入MNIST数据集
+    
+    Parameters
+    ----------
+    normalize : 将图像的像素值正规化为0.0~1.0
+    one_hot_label : 
+        one_hot_label为True的情况下，标签作为one-hot数组返回
+        one-hot数组是指[0,0,1,0,0,0,0,0,0,0]这样的数组
+    flatten : 是否将图像展开为一维数组
+    
+    Returns
+    -------
+    (训练图像, 训练标签), (测试图像, 测试标签)
+    """
+    if not os.path.exists(save_file):
+        init_mnist()
+        
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+    
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+            
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+    
+    if not flatten:
+         for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+
+
+if __name__ == '__main__':
+    init_mnist()
